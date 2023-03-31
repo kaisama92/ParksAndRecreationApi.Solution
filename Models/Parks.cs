65992701de4e96712b77399bb8b3e1c51c129da4
Kai Clausen <kaiclausen123@gmail.com>
2023-03-31 10:02:53 -0700

    add further fields and seed information

--- Models/Parks.cs
+++ Models/Parks.cs
@@ -4,11 +4,13 @@ namespace ParksAndRecreationApi.Models
   {
     public int ParkId { get; set; }
     public string ParkName { get; set; }
+    public bool IsStatePark { get; set; }
+    public bool IsNationalPark { get; set; }
     public string County { get; set; }
     public bool HasBathrooms { get; set; }
     public bool HasFamilyFriendlyBathrooms { get; set; }
     public bool HasPlayStructure { get; set; }
-    public float Latitude { get; set; }
-    public float Longitude { get; set; }
+    public string Latitude { get; set; }
+    public string Longitude { get; set; }
   }
 }
---
+++ Models/ParksAndRecreationApiContext.cs
@@ -0,0 +1,22 @@
+using Microsoft.EntityFrameworkCore;
+
+namespace ParksAndRecreationApi.Models
+{
+  public class ParksAndRecreationApiContext : DbContext
+  {
+    public DbSet<Quote> Quotes { get; set; }
+
+    public ParksAndRecreationApiContext(DbContextOptions<ParksAndRecreationApiContext> options) : base(options)
+    {
+    }
+
+    protected override void OnModelCreating(ModelBuilder builder)
+    {
+      builder.Entity<Park>().HasData(
+        new Park { ParkId = 1, ParkName = "Willamette Stone State Heritage Site", IsStatePark = true, HasBathrooms = false, HasFamilyFriendlyBathrooms = false, HasPlayStructure = false, Latitude = "45.52075 N", Longitude = "122.74365 W"},
+        new Park { ParkId = 2, ParkName = "Wapato Access Greenway State Park", IsStatePark = true, HasBathrooms = false, HasFamilyFriendlyBathrooms = false, HasPlayStructure = false, Latitude = "45.66018 N", Longitude = "122.83870 W"},
+        new Park { ParkId = 3, ParkName = "Fort Vanouver National Historic Site", IsNationalPark = true, HasFamilyFriendlyBathrooms = true, HasPlayStructure = false, Latitude = "45.62280 N", Longitude = "122.66215 W"}
+        );
+    }
+  }
+}
